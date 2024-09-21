5491db49b154dfff87a208bbe949bdf02a263108
KutuevTimur <mr.kreek68@gmail.com>
2024-09-21 14:31:12 +0300

    Remote файлы

--- main11.py
+++
@@ -1,5 +0,0 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-print(f"First number: {num1}, Second number: {num2}")
-result = num1 - num2
-print(f"Razn of {num1} and {num2} is {result}")
