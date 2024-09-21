f4d0bd347cf8d874bf3fcaf1291594fc9718e500
KutuevTimur <mr.kreek68@gmail.com>
2024-09-21 14:04:23 +0300

    Add sum operation

--- main11.py
+++ main11.py
@@ -1,3 +1,5 @@
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 print(f"First number: {num1}, Second number: {num2}")
+result = num1 + num2
+print(f"Sum of {num1} and {num2} is {result}")
